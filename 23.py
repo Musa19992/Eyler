e1ce28b362988e69dc52eed21f9e50f8e2839990
Musa19992 <abdurahmanov.musa@mail.ru>
2019-07-08 22:51:25 +0300

    first commit

--- 23.py
+++ 23.py
@@ -21,27 +21,5 @@ for k in range(len(lst2) - 1):
     for t in range(k + 1, len(lst2)):
         lst3.append(lst2[k] + lst2[t])
 print(sum([i for i in range(1, N) if i not in lst3]))
-"""def compute():
-    LIMIT = 29
-    divisorsum = [0] * LIMIT
-    for i in range(1, LIMIT):
-        for j in range(i * 2, LIMIT, i):
-            divisorsum[j] += i
-    abundantnums = [i for (i, x) in enumerate(divisorsum) if x > i]
-
-    expressible = [False] * LIMIT
-    for i in abundantnums:
-        for j in abundantnums:
-            if i + j < LIMIT:
-                expressible[i + j] = True
-            else:
-                break
-
-    ans = sum(i for (i, x) in enumerate(expressible) if not x)
-    return str(ans)
-
-
-if __name__ == "__main__":
-    print(compute())"""
 
 
