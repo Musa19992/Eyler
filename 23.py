4ac7262ac3f25136c93c1733404f8ff62219ef40
Musa19992 <abdurahmanov.musa@mail.ru>
2019-07-09 01:15:54 +0300

    first commit

--- 23.py
+++ 23.py
@@ -1,12 +1,14 @@
-N = 28123
+import math
+N = 28124
 lst2 = []
 lst3 = []
-lst4 = []
 def sumdel(i):
     sum = 1
     for g in range (2, int(i**0.5) + 1):
         if i % g == 0:
-            sum += g + (i//g)
+            sum += g + (i/g)
+        if i / g == math.sqrt(i):
+            sum = sum - g
     return sum
 
 def proverka(i):
@@ -16,10 +18,11 @@ def proverka(i):
 
 for i in range(1, N):
     proverka(i)
-
-for k in range(len(lst2) - 1):
-    for t in range(k + 1, len(lst2)):
-        lst3.append(lst2[k] + lst2[t])
-print(sum([i for i in range(1, N) if i not in lst3]))
+print(lst2)
+for i in range(len(lst2)):
+    for g in range(i, len(lst2)):
+        lst3.append(lst2[i] + lst2[g])
+lst3 = sorted(set(lst3))
+print(sum([t for t in range(N) if t not in lst3]))
 
 
