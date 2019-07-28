6af898225ac4c7f3065f62f863990c54bf026cf7
Musa19992 <abdurahmanov.musa@mail.ru>
2019-07-28 14:43:27 +0300

    "first commit"

--- 19.py
+++ 19.py
@@ -1,4 +1,3 @@
-DAYS = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"]
 y = 1
 count = 0
 for i in range(1901, 2001):
@@ -16,10 +15,9 @@ for i in range(1901, 2001):
         else:
             k_dney = 30
         for t in range(k_dney):
-            if (t == 0) and DAYS[y] == "SUN":
+            if (t == 0) and y == 6:
                 count += 1
             y += 1
             if y == 7:
                 y = 0
 print(count)
-
