38a777ef2301596ffed150fceafdde7848917f12
Musa19992 <abdurahmanov.musa@mail.ru>
2019-07-28 14:07:42 +0300

    "first commit"

--- 19.py
+++ 19.py
@@ -1,5 +1,27 @@
-a = "001000001110001000"
-a1 = sorted(a.split("1"))[-1]
-s1 = a.find(a1)
-final_spisok = (a[s1 - 1 : s1 + len(a1) + 1])
-print(final_spisok)
+MONTH = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"]
+k_dney = 0
+DAYS = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"]
+y = 1
+count = 0
+for i in range(1901, 2001):
+    for g in range(1, 13):
+        if (g == 2) and (i%4 == 0):
+            k_dney = 29
+        elif(g == 2):
+            k_dney = 28
+        elif (g == 8) or (g == 10) or (g == 12):
+            k_dney = 31
+        elif (g == 9) or (g == 11):
+            k_dney = 30
+        elif (g%2 != 0):
+            k_dney = 31
+        else:
+            k_dney = 30
+        for t in range(k_dney):
+            if (t == 0) and DAYS[y] == "SUN":
+                count += 1
+            y += 1
+            if y == 7:
+                y = 0
+print(count)
+
