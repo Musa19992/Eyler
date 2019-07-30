b5150e0749f8fed75a6bd19785698efa50592700
Musa19992 <abdurahmanov.musa@mail.ru>
2019-07-30 20:22:02 +0300

    "first commit"

---
+++ 55.py
@@ -0,0 +1,17 @@
+import time
+start = time.time()
+def Lich(i):
+    for g in range(50):
+        i = (i + int(str(i)[::-1]))
+        if palind(i) == True:
+            return True
+            break
+    return False
+
+def palind(i):
+    if str(i) == str(i)[::-1]:
+        return True
+    else:
+        return False
+print(sum([1 for i in range(1, 10000) if Lich(i) == False]))
+print(time.time() - start)
