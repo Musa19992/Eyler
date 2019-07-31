759d1e7fd649c50debdeae302b599711f4a50273
Musa19992 <abdurahmanov.musa@mail.ru>
2019-07-31 23:56:42 +0300

    "first commit"

---
+++ 56.py
@@ -0,0 +1,12 @@
+import time
+start = time.time()
+def summa(i):
+    return(sum([int(g) for g in str(i)]))
+maximum = 0
+for t in range(1, 100):
+    for k in range(1, 100):
+        r = summa(t**k)
+        if r > maximum:
+            maximum = r
+print(maximum)
+print(time.time() - start)
