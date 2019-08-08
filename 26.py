8b846e959825eda39c86d26743c70646e761cfab
Musa19992 <abdurahmanov.musa@mail.ru>
2019-08-08 18:09:12 +0300

    "first commit"

--- 26.py
+++ 26.py
@@ -1,17 +1,20 @@
+from math import sqrt
 def toFixed(numObj, digits=0):
     return f"{numObj:.{digits}f}"
 def funct(i):
     k = 0
     posol = ""
     zet = str(toFixed(1/i, 100))
-    for t in zet[2::]:
+    for t in zet[3::]:
         posol += t
-        if posol == zet[2+len(posol):2 + 2*len(posol):] or zet[2 + k:5 + k:] == zet[2 + 3 + k:5 + 3 + k:]:
+        if posol == zet[3 + len(posol):3 + 2*len(posol):] or zet[5:5 + 2:] == zet[7:7 + 2:]:
             break
         k += 1
     return len(posol)
 max = 0
+otvet = 0
 for i in range(1, 999):
     if (funct(i) > max):
-        max = i
-print(max)
+        otvet = i
+        max = funct(i)
+print(otvet)
