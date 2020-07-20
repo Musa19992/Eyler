3140452cd6cf0b01fc4f811403fc0a2d8e869cd3
Муса <abdurahmanov.musa@mail.ru>
2020-07-20 19:19:54 +0300

    second commit

--- 5.py
+++ 5.py
@@ -1,15 +1,17 @@
 import time
 start=time.time()
-lst = [9, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+lst = [11, 13, 14, 15, 16, 17, 18, 19, 20]
 min = 0
 number = 0
 while 1 == 1:
     number += 20
     k = 0
-    for i in range (0, 11):
+    for i in range (0, 8):
         if(number % lst[i] != 0):
-            k = k + 1
-    if (k == 0):
+            break
+        else:
+            k += 1
+    if (k == 8):
         min = number
         break
 print("min = %d" %min)
